b7a55117600f4371c0c335626246560b7b23f9d0
XYF <914139745@qq.com>
2024-03-27 15:34:26 +0800

    Struct Change
    更改项目结构，添加命名空间

---
+++ Assets/Scripts/System/Cameras/CinemachineSetting/CinemachineInputAction.cs
@@ -0,0 +1,21 @@
+using Cinemachine;
+using UnityEngine;
+
+namespace OpenWorld.System
+{
+    public class CinemachineInputAction : MonoBehaviour, AxisState.IInputAxisProvider
+    {
+        public float GetAxisValue(int axis)
+        {
+            if (axis == 0)
+            {
+                return GameManager.CameraMoveInput.x;
+            }
+            if (axis == 1)
+            {
+                return GameManager.CameraMoveInput.y;
+            }
+            return 0;
+        }
+    }
+}
